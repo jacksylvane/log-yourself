0741628350e1436e69b40aa32c1a71b56bfe678d
Michal <sisakm@hotmail.sk>
2018-03-09 11:01:16 +0100

    ISO week implemented

--- src/app/pipes/week.pipe.ts
+++ src/app/pipes/week.pipe.ts
@@ -6,19 +6,20 @@ export class WeekPipe implements PipeTransform {
     return this.getWeekNumber(value);
   }
 
-  // source: http://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
-  private getWeekNumber(d: Date): number {
+  // ISO WEEK - source: http://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+  getWeekNumber(d: Date): number {
     // Copy date so don't modify original
-    d = new Date(+d);
-    d.setHours(0, 0, 0);
+    let x = new Date(+d);
+    x.setHours(0, 0, 0);
+    x = new Date(Date.UTC(x.getFullYear(), x.getMonth(), x.getDate()));
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
-    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+    x.setUTCDate(x.getUTCDate() + 4 - (x.getUTCDay() || 7));
     // Get first day of year
-    const yearStart = new Date(d.getFullYear(), 0, 1);
+    const yearStart = new Date(Date.UTC(x.getUTCFullYear(), 0, 1));
     // Calculate full weeks to nearest Thursday
-    const weekNo = Math.ceil((((d.valueOf() - yearStart.valueOf()) / 86400000) + 1) / 7);
-    // Return array of year and week number
+    const weekNo = Math.ceil((((x.valueOf() - yearStart.valueOf()) / 86400000) + 1) / 7);
+    // Return week number
     return weekNo;
   }
 }
